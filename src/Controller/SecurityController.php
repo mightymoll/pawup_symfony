1b661e0854b4d6051803a2e17923bc11e9ffbd57
molly <mnfelts@gmail.com>
2024-01-18 15:06:50 +0100

    Sign in for public User
    - added /signup route to security controller
      - set role as 'ROLE_USER' in DB
    - creation of a signup form
    - twig template to display form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,11 +4,46 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\ORM\EntityManagerInterface;
+
+// for hashing password
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+// import User entity & form
+use App\Entity\User;
+use App\Form\SignupType;
 
 class SecurityController extends AbstractController
 {
+    #[Route('/signup', name: 'app_user_signup', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hashpwd): Response
+    {
+        $user = new User();
+        $form = $this->createForm(SignupType::class, $user);
+        $form->handleRequest($request);
+
+        // when form is submitted & valid, store in DB as new User with hashed password
+        if ($form->isSubmitted() && $form->isValid()) {
+            // hash password before putting in DB
+            $user->setPassword($hashpwd->hashPassword($user, $form->get('password')->getData()));
+            // set role as public user (ROLE_USER)
+            $user->setRoles(["ROLE_USER"]);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('security/signup.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
